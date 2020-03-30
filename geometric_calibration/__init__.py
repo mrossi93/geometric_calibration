fd97075208ca6bdee0e17635f2d923a397f80789
Matteo Rossi <rossimatteo1993@gmail.com>
2020-03-30 20:05:58 +0200

    Fixed little bug in bumpversion

--- geometric_calibration/__init__.py
+++ geometric_calibration/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Geometric Calibration."""
 
 __author__ = """Matteo Rossi"""
-__email__ = 'rossimatteo1993@gmail.com'
-__version__ = '0.1.0'
+__email__ = "rossimatteo1993@gmail.com"
+__version__ = "0.1.0"
