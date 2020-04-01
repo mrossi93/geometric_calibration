3307f2349e5006a98c5ae303dedd2a00f989b950
Matteo Rossi <rossimatteo1993@gmail.com>
2020-04-01 16:16:25 +0200

    Minor changes

--- tests/test_geometric_calibration.py
+++ tests/test_geometric_calibration.py
@@ -32,6 +32,6 @@ def test_command_line_interface():
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     # assert 'geometric_calibration.cli.main' in result.output
-    # help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
     # assert '--help  Show this message and exit.' in help_result.output
